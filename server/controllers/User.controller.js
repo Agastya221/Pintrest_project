691635c5d764a3a707f483da4963122e862495b8
agastya221 <agastyash213@gmail.com>
2024-02-10 12:47:17 +0530

    fixed the bugs

---
+++ server/controllers/User.controller.js
@@ -0,0 +1,10 @@
+import User from "../models/User.models.js"
+
+const RegisterUser = async()=>{
+
+    const { username, fullName, avatar, email, password } = req.body;
+    
+
+
+
+}
--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 connectDB()
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
